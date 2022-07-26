942c4931a5d9c83a4368f40560302d9b181f2fb5
IJA <alekajobs@gmail.com>
2022-07-26 14:44:33 -0700

    password and confirm password js now works, button now responds according to inputs

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 let passwordInput = document.getElementById("password");
 let confirmPasswordInput = document.getElementById("confirm-password");
-
-
+let passwordMessage = document.getElementById("message2");
+let createAccountButton = document.getElementById("create-account");
+let form = document.getElementById("my-form");
 
 let confirmPassword = () => {
-    if (passwordInput.textContent !== confirmPasswordInput.textContent) {
-        confirmPasswordInput.style.borderColor = "red";
+    if (confirmPasswordInput.value === "") {
+        return;
+    }
+    else if (passwordInput.value !== confirmPasswordInput.value) {
+        confirmPasswordInput.style.borderColor = "#ff000080";
         confirmPasswordInput.title = "password did not match";
+        passwordMessage.innerText = "passwords do not match";
+        return false;
     }
-    else if (passwordInput.textContent !== confirmPasswordInput.textContent) {
-        confirmPasswordInput.style.borderColor = "green";
+    else if (passwordInput.value === confirmPasswordInput.value) {
+        confirmPasswordInput.style.borderColor = "#00800080";
+        passwordMessage.innerText = "passwords match";
+        return true;
     }
 }
 
+confirmPasswordInput.addEventListener("keyup", () => {
+    createAccountButton.disabled = false;
+    if (passwordInput.value !== confirmPasswordInput.value) {
+        createAccountButton.disabled = true;
+    }
+})
 
-let main = () => {
-    confirmPassword();
-}
-
-main();
+confirmPassword();
